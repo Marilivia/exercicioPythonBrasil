32f3a9a8806061e5f80422d65b1ecf9dce0db64a
marilivia <mariliviamelo@hotmail.com>
2019-04-28 19:51:26 -0300

    implementado metodo alterar fome

--- exercicio_POO/exercicio07.py
+++ exercicio_POO/exercicio07.py
@@ -19,26 +19,56 @@ então não devemos criar um atributo para armazenar esta informação por que e
 >>> t1.alterarNome('Joao')
 >>> t1.Nome
 'Joao'
+>>> t1.sensacaoFome
+Faminto
+>>> t1.alterarFome()
+>>> t1.sensacaoFome
+com muita fome
+>>> t1.alterarFome()
+>>> t1.sensacaoFome
+fome
+>>> t1.alterarFome()
+>>> t1.sensacaoFome
+pensando em comida
+>>> t1.alterarFome()
+>>> t1.sensacaoFome
+com energia suficiente
+>>> t1.alterarFome()
+>>> t1.sensacaoFome
+satisfeito
+>>> t1.alterarFome()
+>>> t1.sensacaoFome
+cheio
+
+>>> t1.retornar_fome()
+>>> t1.Fome
+7
 """
+escalaFome = ['Faminto', ' com muita fome', 'fome', 'Pensando em comida',
+                  'com energia suficiente', 'satisfeito', 'cheio']
 class BichinhoVirtual:
-    def __init__(self,nome_bichinho : str):
+
+
+    def __init__(self, nome_bichinho : str):
         self.Nome = nome_bichinho
         self.Fome = 0
+        self.sensacaoFome = escalaFome[self.Fome]
         self.Saude = 0
         self.Idade = 0
 
     def alterarNome(self,novo_nome):
        self.Nome = novo_nome
     def alterarFome(self):
-        pass
+      self.Fome = (self.Fome + 1)%len(escalaFome)
+      self.sensacaoFome = escalaFome[self.Fome]
     def alterarSaude(self):
         pass
     def alterarIdade(self):
         pass
-    def retornar_nome(self):
-        pass
+    def retornarNome(self):
+        print(self.Nome)
     def retornar_fome(self):
-        pass
+        print(self.Fome)
     def retornar_saude(self):
         pass
     def retornar_idade(self):
